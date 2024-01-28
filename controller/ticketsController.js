0edb9c99ad114b1a73eb70e63c1111a6f9ddef1e
JoyCode4 <joywadhonkar4@gmail.com>
2024-01-28 22:04:43 +0530

    cerated endpoint update in ticket purchase and created new endpoint to get all purchased tickets by that user

--- controller/ticketsController.js
+++ controller/ticketsController.js
@@ -1,7 +1,7 @@
 import { User } from "../model/User.js";
 import { Ticket } from "../model/Ticket.js";
 
-export const create = async (req, res) => {
+export const purchaseTicket = async (req, res) => {
   const { ticketType, price, quantity } = req.body;
   const { event } = req.params;
   const user = await User.findById(req.userId);
@@ -22,6 +22,21 @@ export const create = async (req, res) => {
     });
   } catch (err) {
     return res.send({
+      error: err.message,
+    });
+  }
+};
+
+export const getTickets = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId).populate("tickets");
+    const tickets = user.tickets;
+    res.send({
+      message: "get all purchased Tickets",
+      tickets,
+    });
+  } catch (err) {
+    res.send({
       error: err.message,
     });
   }
--- routes/api/tickets.js
+++ routes/api/tickets.js
@@ -1,6 +1,10 @@
 import express from "express";
-import { create } from "../../controller/ticketsController.js";
+import {
+  purchaseTicket,
+  getTickets,
+} from "../../controller/ticketsController.js";
 import { auth } from "../../middleware/jwtauth.js";
 export const ticketsRouter = express.Router();
 
-ticketsRouter.post("/create/:event", auth, create);
+ticketsRouter.post("/purchase/:event", auth, purchaseTicket);
+ticketsRouter.get("/", auth, getTickets);
