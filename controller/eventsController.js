f8e9b3838b7371c67801e7d54c433508fc8b85ff
JoyCode4 <joywadhonkar4@gmail.com>
2024-01-28 21:47:25 +0530

    create end point to get all events and single event details

--- controller/eventsController.js
+++ controller/eventsController.js
@@ -2,9 +2,9 @@ import { User } from "../model/User.js";
 import { Event } from "../model/Event.js";
 
 export const create = async (req, res) => {
-  const user = await User.findById(req.userId);
   const { title, description, date, time, location } = req.body;
   try {
+    const user = await User.findById(req.userId);
     const event = await Event.create({
       title,
       description,
@@ -17,6 +17,35 @@ export const create = async (req, res) => {
     user.events.push(event);
     user.save();
     return res.send({ message: "Event created successfully", event: event });
+  } catch (err) {
+    return res.send({
+      error: err.message,
+    });
+  }
+};
+
+export const allEvents = async (req, res) => {
+  try {
+    const events = await Event.find();
+    return res.send({
+      message: "All Events",
+      events: events,
+    });
+  } catch (err) {
+    return res.send({
+      error: err.message,
+    });
+  }
+};
+
+export const eventById = async (req, res) => {
+  const { eventid } = req.params;
+  try {
+    const event = await Event.findById(eventid);
+    return res.send({
+      message: "get event details",
+      event: event,
+    });
   } catch (err) {
     return res.send({
       error: err.message,
--- routes/api/events.js
+++ routes/api/events.js
@@ -1,6 +1,12 @@
 import express from "express";
-import { create } from "../../controller/eventsController.js";
+import {
+  create,
+  allEvents,
+  eventById,
+} from "../../controller/eventsController.js";
 import { auth } from "../../middleware/jwtauth.js";
 export const eventsRouter = express.Router();
 
 eventsRouter.post("/create", auth, create);
+eventsRouter.get("/all_events", auth, allEvents);
+eventsRouter.get("/:eventid", auth, eventById);
